cd8c18fdf5a6e3ee5a3476e7c363dd1e5d7d9131
CuongNguyen3110 <cuongnguyen1.fit@gmail.com>
2018-11-26 15:54:20 +0700

    intial commit

--- Web2/homework/ex.py
+++ Web2/homework/ex.py
@@ -6,10 +6,10 @@ mlab.connect()
 #ex2
 africa_river_list = River.objects(continent__icontains="Africa")
 for a in africa_river_list:
-    print(a.name)
-
+    print(a.name, end=', ')
+print()
 #ex3
-america_river_list = River.objects(continent__icontains="S.America", length__lt=1000)
+america_river_list = River.objects(continent__icontains="America", length__lt=1000)
 for a in america_river_list:
-    print(a.name)
+    print(a.name, end=', ')
 
